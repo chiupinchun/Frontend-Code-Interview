b62eb19bf8621da2507f6130fe518c904203c5f1
Hato <fansintuesan@gmail.com>
2024-04-26 12:25:37 +0800

    feat: rate & cheese could appear at initial position

--- src/pages/find-the-cheese.tsx
+++ src/pages/find-the-cheese.tsx
@@ -1,14 +1,25 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import type { GetStaticProps } from 'next'
 import { request } from '@/utils/request'
 import { twMerge } from 'tailwind-merge'
+import { FaCheese } from 'react-icons/fa'
+import { LuRat } from 'react-icons/lu'
+
+const BLOCK_WIDTH = 25
 
 const BasicBlock: FC<{
   className?: string
+  style?: React.CSSProperties
   children?: React.ReactNode
-}> = ({ className = '', children }) => {
+}> = ({ className = '', style = {}, children }) => {
   return (
-    <div className={twMerge('w-5 h-5', className)}>
+    <div
+      className={twMerge(className, 'flex justify-center items-center')}
+      style={Object.assign({
+        width: `${BLOCK_WIDTH}px`,
+        height: `${BLOCK_WIDTH}px`
+      }, style)}
+    >
       {children}
     </div>
   )
@@ -26,16 +37,30 @@ const Path: FC = () => {
   )
 }
 
-// TODO: what is RuRat & FaCheese?
-const Rate: FC = () => {
+interface Coordinate {
+  x: number
+  y: number
+}
+
+const Rate: FC<{ coordinate: Coordinate }> = ({ coordinate }) => {
   return (
-    <BasicBlock>M</BasicBlock>
+    <BasicBlock className='absolute' style={{
+      left: coordinate.x * BLOCK_WIDTH,
+      top: coordinate.y * BLOCK_WIDTH
+    }}>
+      <LuRat />
+    </BasicBlock>
   )
 }
 
-const Cheese: FC = () => {
+const Cheese: FC<{ coordinate: Coordinate }> = ({ coordinate }) => {
   return (
-    <BasicBlock>C</BasicBlock>
+    <BasicBlock className='absolute' style={{
+      left: coordinate.x * BLOCK_WIDTH,
+      top: coordinate.y * BLOCK_WIDTH
+    }}>
+      <FaCheese />
+    </BasicBlock>
   )
 }
 
@@ -50,7 +75,7 @@ type MapData = (keyof typeof blockEnum)[][]
 
 export const getStaticProps = (async (context) => {
   const { data: maps } = await request.get('/api/maze')
-    .catch((err) => {
+    .catch(() => {
       // record error log if needed.
       return { data: [] }
     })
@@ -63,19 +88,38 @@ export const getStaticProps = (async (context) => {
 const Map: FC<{
   data: MapData
 }> = ({ data }) => {
+  const [rateCoordinate, setRateCoordinate] = useState({ x: 0, y: 0 })
+  const [cheeseCoordinate, setCheeseCoordinate] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    data.forEach((row, y) => {
+      row.forEach((item, x) => {
+        switch (item) {
+          case 'start':
+            setRateCoordinate({ x, y })
+            break
+          case 'end':
+            setCheeseCoordinate({ x, y })
+            break
+        }
+      })
+    })
+  }, [data])
 
   return (
-    <div className='flex flex-col items-center'>
+    <div className='relative mx-auto w-fit'>
       {
         data.map((row, index) => (
           <div key={index} className='flex'>
             {row.map((type, index) => {
               const Block = blockEnum[type]
-              return <Block key={index} />
+              return <Block key={index}></Block>
             })}
           </div>
         ))
       }
+      <Rate coordinate={rateCoordinate} />
+      <Cheese coordinate={cheeseCoordinate} />
     </div>
   )
 }
