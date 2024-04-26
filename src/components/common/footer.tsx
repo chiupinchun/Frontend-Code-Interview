fe8ee501ab53dd0a3c0879e49a9bac0eb831138f
Hato <fansintuesan@gmail.com>
2024-04-26 15:35:46 +0800

    feat: basic common layout

---
+++ src/components/common/footer.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react'
+
+interface Props { }
+
+const Footer: FC<Props> = () => {
+  return (
+    <>footer</>
+  )
+}
+
+export default Footer
---
+++ src/components/common/header.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react'
+
+interface Props { }
+
+const Header: FC<Props> = () => {
+  return (
+    <>header</>
+  )
+}
+
+export default Header
